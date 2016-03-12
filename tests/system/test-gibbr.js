a1f3914b56c608ecca93efc602bf13a29f0ce138
rgrannell1 <r.grannell2@gmail.com>
2016-03-12 18:02:07 +0000

    updated test case

--- tests/system/test-gibbr.js
+++ tests/system/test-gibbr.js
@@ -10,8 +10,22 @@ const dirSchema = require('./dir-schema')
 
 
 
-console.log(JSON.stringify(
-	dirSchema.randomSchema(10, 0.05),
-	null,
-	4
-))
+const TEST_CASES = 10
+
+
+
+
+
+for (let ith = 0; ith < TEST_CASES; ++ith) {
+
+	var schema = dirSchema.randomSchema(10, 0.05)
+
+	dirSchema.execSchema(process.cwd( ), schema)
+
+	setTimeout(( ) => {
+
+
+
+	}, 10 * 1000)
+
+}
